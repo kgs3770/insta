d3dc411db9ab61c512b0c41b1b6866062575c145
kkk <kgs3770@gmail.com>
2025-03-31 10:01:28 +0900

    image setting

--- posts/apps.py
+++ posts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PotsConfig(AppConfig):
+class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pots'
+    name = 'posts'
