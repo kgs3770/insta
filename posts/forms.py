a49e62a7771503148b8fad105ca9dc7777cdc07f
kkk <kgs3770@gmail.com>
2025-04-01 10:45:00 +0900

    comment create 기능 구현

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(ModelForm):
     class Meta():
         model = Post
         # fields = '__all__'
         fields = ('content', 'image', )
+
+
+class CommentForm(ModelForm):
+    class Meta():
+        model = Comment
+        fields = ('content', )
